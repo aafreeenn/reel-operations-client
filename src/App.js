37f5928fd2acb44101e115ed0302554163904c83
aafreeenn <aafreenniha@gmail.com>
2025-07-15 13:02:09 +0400

    Fix : added the missing import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Timeslot from './components/Timeslot';
