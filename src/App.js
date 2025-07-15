d5c11a878e9d10561e02e1cc031388031418ed21
aafreeenn <aafreenniha@gmail.com>
2025-07-15 13:29:42 +0400

    Fix : changed UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Timeslot from './components/Timeslot';
